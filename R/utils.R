a34681b87d265f68ecdc5caebe66a2afd6af4f80
shians <registertonysu@gmail.com>
2024-05-21 15:25:35 +1000

    Removed unused function

--- R/utils.R
+++ R/utils.R
@@ -62,12 +62,6 @@ load_example_modbamresult <- function() {
     )
 }
 
-.get_ggplot_range_x <- function(x) {
-    # get x-axis range from a ggplot object
-    # returns c(x_min, x_max)
-    ggplot2::ggplot_build(x)$layout$panel_scales_x[[1]]$range$range
-}
-
 # create a list where the nth element contains the nth values of the original
 # vectors
 #' @importFrom stats setNames

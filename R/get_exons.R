65ca5a69b8d59210fd07b5eec2b5abc1d154a81c
shians <registertonysu@gmail.com>
2024-08-23 12:17:31 +1000

    Fixed handling of missing packages

--- R/get_exons.R
+++ R/get_exons.R
@@ -351,13 +351,16 @@ package_check <- function(packages, req_versions) {
     for (i in seq_along(packages)) {
         package <- packages[i]
         req_version <- req_versions[i]
-        if (package %in% utils::installed.packages()) {
-            if (packageVersion(package) < req_version) {
-                any_missing <- TRUE
-                message(glue::glue(
-                    "required package '{package} (>= {req_version})' is not installed, please install using BiocManager::install({package})"
-                ))
-             }
+        if (!package %in% utils::installed.packages()) {
+            any_missing <- TRUE
+            message(glue::glue(
+                "required package '{package} (>= {req_version})' is not installed, please install using BiocManager::install(\"{package}\")"
+            ))
+        } else if (packageVersion(package) < req_version) {
+            any_missing <- TRUE
+            message(glue::glue(
+                "required package '{package} (>= {req_version})' is not installed, please install using BiocManager::install(\"{package}\")"
+            ))
         }
     }
 
